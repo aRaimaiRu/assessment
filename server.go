9a71957124c93331b37832e16af9b0ac3126f40d
Kittipat <first1801@hotmail.com>
2022-12-21 23:25:30 +0700

    edit run_command and init echo

--- server.go
+++ server.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	e := echo.New()
+	e.Use(middleware.Logger())
+	port := os.Getenv("PORT")
+	log.Fatal(e.Start(port))
+
 }

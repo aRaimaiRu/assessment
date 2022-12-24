32c22773e603b01419e1d9de9fce7ce6c2d01a3a
Kittipat <first1801@hotmail.com>
2022-12-24 20:28:56 +0700

    add update row by id service and unit test

---
+++ expense/update_row.go
@@ -0,0 +1,18 @@
+package expense
+
+import "github.com/lib/pq"
+
+func UpdateRowById(db DBQuery, ex Expense, id int) (Expense, error) {
+	stmt, err := db.Prepare("UPDATE expenses SET title=$2 amount=$3 note=$4 tags=$5  where id=$1")
+	// ex := Expense{}
+	if err != nil {
+		return ex, err
+	}
+
+	_, err = stmt.Exec(id, ex.Id, ex.Title, ex.Amount, ex.Note, pq.Array(&ex.Tags))
+	if err != nil {
+		return ex, err
+	}
+	return ex, nil
+
+}
---
+++ expense/update_row_test.go
@@ -0,0 +1,48 @@
+package expense_test
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	"github.com/aRaimaiRu/assessment/expense"
+)
+
+type Dummy struct {
+}
+type MyError struct {
+	err string
+}
+
+func (m MyError) Error() string {
+	return m.err
+}
+func (d Dummy) QueryRow(query string, args ...any) *sql.Row {
+	return &sql.Row{}
+}
+func (d Dummy) Prepare(query string) (*sql.Stmt, error) {
+	return nil, MyError{"MyError"}
+}
+
+func TestUpdateOneRow(t *testing.T) {
+	t.Run(" PrepareReturnErrorShouldReturnError", func(t *testing.T) {
+
+		give := &Dummy{}
+		want := "MyError"
+		ex := expense.Expense{
+			Title:  "strawberry smoothie",
+			Amount: 79,
+			Note:   "night market promotion discount 10 bath",
+			Tags:   []string{"food", "beverage"},
+		}
+
+		fmt.Printf("expense =>%v\n", ex)
+		fmt.Printf("give =>%v\n", give)
+		_, err := expense.QueryExpense(give, 1)
+
+		if err.Error() != want {
+			t.Errorf("expect: %v got: %v", want, err.Error())
+		}
+	})
+
+}
